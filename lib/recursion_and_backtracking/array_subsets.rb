90cc73e885aeb618cab7e2bac6c04cbd15c5fe2f
jeanpaulsio <sio.jeanpaul@gmail.com>
2019-07-02 19:21:21 -0700

    updates practice

--- lib/recursion_and_backtracking/array_subsets.rb
+++ lib/recursion_and_backtracking/array_subsets.rb
@@ -6,21 +6,6 @@
 
 class ArraySubsets
   def self.run(arr, buffer, next_index, buffer_index)
-    return "" if arr.empty? || buffer.length > arr.length
-
-    print_buffer(buffer, buffer_index)
-    if buffer.length <= buffer_index
-      return
-    end
-
-    if arr.length == next_index
-      return
-    end
-
-    next_index.upto(arr.length - 1) do |i|
-      buffer[buffer_index] = arr[i]
-      run(arr, buffer, i + 1, buffer_index + 1)
-    end
   end
 
   private
